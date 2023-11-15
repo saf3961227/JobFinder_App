05e59c91970ac8a4ffaa48857304d2c09d084bb5
Ahmed Shams Saif <ahmedshams3572@gmail.com>
2023-11-16 00:57:08 +1100

    upload

---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import profile from "../assets/images/ahmed.jpg";
+
+export default {
+  profile,
+};
---
+++ hook/useFetch.js
@@ -0,0 +1,47 @@
+import { useEffect, useState, userEffect } from 'react';
+import axios from 'axios';
+
+
+const useFetch = (endpoint, query) => {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        headers: {
+            'X-RapidAPI-Key': 'b4703ee5abmsh193c1273dbaa4c4p1efdfejsn99327bb66898',
+            'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        },
+        params: { ...query },
+      };
+
+    const fetchData = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await axios.request(options);
+            setData(response.data.data);
+            setIsLoading(false);
+        } catch (error) {
+            setError(error);
+            alert('There is an error')
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+    }
+
+    return { data, isLoading, error, refetch }
+}
+
+export default useFetch;
